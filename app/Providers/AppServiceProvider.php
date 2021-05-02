0e84ab4f37a1f6067336312f52d1ca79c0481021
cadd70 <cadd70@gmail.com>
2021-05-02 01:05:58 -0300

    forca https para assets - configurao no arquivo appserviceprovider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(config('app.env') === 'producao') {
+            \URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
     }
 }
