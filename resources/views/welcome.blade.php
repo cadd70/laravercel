fa8229dd1de664c041315c5286eac4fcf339d741
Carlos Dalcin <48837300+cadd70@users.noreply.github.com>
2021-05-02 09:40:27 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 
         <!-- Styles -->
 
-        <link rel="stylesheet" href="{{ asset(/css/bootstrap.min.css) }}">
+        <link rel="stylesheet" href="../../public/css/bootstrap.min.css">
         
         <style>
             html, body {
