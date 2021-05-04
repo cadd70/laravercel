269cad66ae650941790e3c2aa5d6b2937fe4b205
cadd70 <cadd70@gmail.com>
2021-05-04 16:40:26 -0300

    corrige responsividade do footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,13 +12,11 @@
         <style>
             .content { 
                 margin-top: 70px; 
-                margin-bottom: 110px;
             }
 
             @media screen and (max-width: 768px) {
                 .content { 
                     margin-top: 110px; 
-                    margin-bottom: 130px;
                 }
             }
         </style>
@@ -51,7 +49,7 @@
             <div class="content">
                 <div class="row text-center">
 
-                    <div class="card mb-3">
+                    <div class="card mb-3 col-12">
                         <div class="card-header bg-dark text-white">
                             <h1>Deputado Federal <br class="d-md-none"><b>Carlos Alberto</b> <br class="d-lg-none"><small>Partido: <b>WXZY</b></small></h1>
                         </div>
@@ -68,7 +66,95 @@
                             </div>
                         </div>
                     </div>
+
+                    <div class="card mb-3 col-12">
+                        <div class="card-header bg-dark text-white">
+                            <h1>Deputado Federal <br class="d-md-none"><b>Carlos Alberto</b> <br class="d-lg-none"><small>Partido: <b>WXZY</b></small></h1>
+                        </div>
+                        <div class="row no-gutters">
+                            <div class="col-md-3">
+                                <img height="250" width="180" src="{{ asset('images/no-user-picture.jpg') }}" alt="no-user-picture">
+                            </div>
+                            <div class="col-md-9">
+                                <div class="card-body">
+                                    <h3 class="card-title">Card title</h3>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3 col-12">
+                        <div class="card-header bg-dark text-white">
+                            <h1>Deputado Federal <br class="d-md-none"><b>Carlos Alberto</b> <br class="d-lg-none"><small>Partido: <b>WXZY</b></small></h1>
+                        </div>
+                        <div class="row no-gutters">
+                            <div class="col-md-3">
+                                <img height="250" width="180" src="{{ asset('images/no-user-picture.jpg') }}" alt="no-user-picture">
+                            </div>
+                            <div class="col-md-9">
+                                <div class="card-body">
+                                    <h3 class="card-title">Card title</h3>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3 col-12">
+                        <div class="card-header bg-dark text-white">
+                            <h1>Deputado Federal <br class="d-md-none"><b>Carlos Alberto</b> <br class="d-lg-none"><small>Partido: <b>WXZY</b></small></h1>
+                        </div>
+                        <div class="row no-gutters">
+                            <div class="col-md-3">
+                                <img height="250" width="180" src="{{ asset('images/no-user-picture.jpg') }}" alt="no-user-picture">
+                            </div>
+                            <div class="col-md-9">
+                                <div class="card-body">
+                                    <h3 class="card-title">Card title</h3>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3 col-12">
+                        <div class="card-header bg-dark text-white">
+                            <h1>Deputado Federal <br class="d-md-none"><b>Carlos Alberto</b> <br class="d-lg-none"><small>Partido: <b>WXZY</b></small></h1>
+                        </div>
+                        <div class="row no-gutters">
+                            <div class="col-md-3">
+                                <img height="250" width="180" src="{{ asset('images/no-user-picture.jpg') }}" alt="no-user-picture">
+                            </div>
+                            <div class="col-md-9">
+                                <div class="card-body">
+                                    <h3 class="card-title">Card title</h3>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3 col-12">
+                        <div class="card-header bg-dark text-white">
+                            <h1>Deputado Federal <br class="d-md-none"><b>Carlos Alberto</b> <br class="d-lg-none"><small>Partido: <b>WXZY</b></small></h1>
+                        </div>
+                        <div class="row no-gutters">
+                            <div class="col-md-3">
+                                <img height="250" width="180" src="{{ asset('images/no-user-picture.jpg') }}" alt="no-user-picture">
+                            </div>
+                            <div class="col-md-9">
+                                <div class="card-body">
+                                    <h3 class="card-title">Card title</h3>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
+                
                 <br>
             </div>
         </div>
@@ -82,7 +168,7 @@
                     <!--Grid column-->
                     <div class="footer col-sm-12 mb-md-0">
                         <p style="vertical-align: middle;">
-                            <b>Consultoria nas redes sociais:</b> 
+                            <b>Consultoria nas redes sociais:</b> <br class="d-sm-none">
                             <a class="text-dark mx-2" href=""><i class="fab fa-twitter-square fa-2x"></i></a>
                             <a class="text-dark mx-2" href=""><i class="fab fa-facebook-square fa-2x"></i></a>
                             <a class="text-dark mx-2" href=""><i class="fab fa-instagram-square fa-2x"></i></a>
